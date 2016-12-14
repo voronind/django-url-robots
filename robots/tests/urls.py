12d58f01893b20d562b6c4399f93e592b38625da
Josh Owen <josh@openfolio.com>
2016-12-14 13:06:12 -0500

    don't use patterns

--- robots/tests/urls.py
+++ robots/tests/urls.py
@@ -1,10 +1,10 @@
 #coding=utf-8
 
-from django.conf.urls import patterns, include
+from django.conf.urls import include
 
 from robots.utils import url
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^profile', include('robots.tests.urls_profile')),
-)
+]
