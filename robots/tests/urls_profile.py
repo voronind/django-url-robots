4285265683491f9be468ef212721ffce9e815c78
Josh Owen <josh@openfolio.com>
2016-12-14 13:12:33 -0500

    don't use urlpatterns

--- robots/tests/urls_profile.py
+++ robots/tests/urls_profile.py
@@ -1,12 +1,11 @@
 #coding=utf-8
 
-from django.conf.urls import patterns
 
 from robots.utils import url
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^s$', 'view', name='profiles', robots_allow=True),
     url(r'^/(?P<nick>\w+)$', 'view'),
     url(r'^/(?P<nick>\w+)/private', 'view', name='profile_private', robots_allow=False),
     url(r'^/(?P<nick>\w+)/public', 'view', name='profile_public', robots_allow=True),
-    )
+    ]
