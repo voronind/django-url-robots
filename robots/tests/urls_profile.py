834b5c7670f7aaf24c8f7b16242d2bf0e541cb97
Josh Owen <josh@openfolio.com>
2016-12-14 13:16:51 -0500

    make callable dummy view

--- robots/tests/urls_profile.py
+++ robots/tests/urls_profile.py
@@ -4,8 +4,8 @@
 from robots.utils import url
 
 urlpatterns = [
-    url(r'^s$', 'view', name='profiles', robots_allow=True),
-    url(r'^/(?P<nick>\w+)$', 'view'),
-    url(r'^/(?P<nick>\w+)/private', 'view', name='profile_private', robots_allow=False),
-    url(r'^/(?P<nick>\w+)/public', 'view', name='profile_public', robots_allow=True),
+    url(r'^s$', lambda x: (), name='profiles', robots_allow=True),
+    url(r'^/(?P<nick>\w+)$', lambda x: ()),
+    url(r'^/(?P<nick>\w+)/private', lambda x: (), name='profile_private', robots_allow=False),
+    url(r'^/(?P<nick>\w+)/public', lambda x: (), name='profile_public', robots_allow=True),
     ]
